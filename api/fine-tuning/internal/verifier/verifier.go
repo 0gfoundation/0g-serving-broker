66151ff2b6e7d5b2307d3922c1b402be13dce90e
Joel <liuyuanvic@gmail.com>
2025-01-09 11:02:38 +0000

    use ecies to encrypt

--- api/fine-tuning/internal/verifier/verifier.go
+++ api/fine-tuning/internal/verifier/verifier.go
@@ -1,12 +1,10 @@
-package post
+package verifier
 
 import (
 	"bytes"
 	"context"
 	"crypto/ecdsa"
 	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
 	"encoding/gob"
 	"fmt"
 	"math/big"
@@ -18,11 +16,30 @@ import (
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
 	"golang.org/x/crypto/sha3"
 )
 
 const aesKeySize = 32 // 256-bit AES key (32 bytes)
+
+type SignatureMetadata struct {
+	taskFee      *big.Int
+	fileRootHash []byte
+	userAddress  common.Address
+	nonce        *big.Int
+}
+
+func (s *SignatureMetadata) Serialize() ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(s)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
 
 type SettlementMetadata struct {
 	modelRootHash   []byte
@@ -43,10 +60,68 @@ func (s *SettlementMetadata) Serialize() ([]byte, error) {
 
 type Verifier struct {
 	contract *providercontract.ProviderContract
+	users    map[common.Address]*ecdsa.PublicKey
 	logger   log.Logger
 }
 
-func (v *Verifier) Verify(ctx context.Context, sourceDir string, providerPriv *ecdsa.PrivateKey, publicKey *rsa.PublicKey, user common.Address, deliverIndex uint64, taskFee string, nonce string) (*SettlementMetadata, error) {
+func (v *Verifier) PreVerify(ctx context.Context, totalFee *big.Int, signatureHex string, signatureMetadata SignatureMetadata) error {
+	if totalFee.Cmp(signatureMetadata.taskFee) < 0 {
+		return errors.New("not enough task fee")
+	}
+
+	account, err := v.contract.GetUserAccount(ctx, signatureMetadata.userAddress)
+	if err != nil {
+		return err
+	}
+
+	amount := new(big.Int).Sub(account.Balance, account.PendingRefund)
+	if amount.Cmp(signatureMetadata.taskFee) < 0 {
+		return errors.New("not enough balance")
+	}
+
+	if account.Nonce.Cmp(signatureMetadata.nonce) > 0 {
+		return errors.New("invalid nonce")
+	}
+
+	return v.verifyUserSignature(ctx, signatureHex, signatureMetadata)
+}
+
+func (v *Verifier) verifyUserSignature(ctx context.Context, signatureHex string, signatureMetadata SignatureMetadata) error {
+	signature, err := hexutil.Decode(signatureHex)
+	if err != nil {
+		return errors.Wrap(err, "decoding signature")
+	}
+	if len(signature) != 65 {
+		return errors.Wrap(err, "invalid signature length")
+	}
+
+	if signature[64] != 27 && signature[64] != 28 {
+		return errors.Wrap(err, "invalid recovery ID")
+	}
+	signature[64] -= 27
+
+	message, err := signatureMetadata.Serialize()
+	if err != nil {
+		return errors.Wrap(err, "serialize signature metadata")
+	}
+	messageHash := ethcrypto.Keccak256Hash([]byte(message))
+
+	publicKey, err := ethcrypto.SigToPub(messageHash.Bytes(), signature)
+	if err != nil {
+		return errors.Wrap(err, "recovering public key")
+	}
+
+	address := ethcrypto.PubkeyToAddress(*publicKey)
+	if address != signatureMetadata.userAddress {
+		return errors.Wrap(err, "signature verification failed")
+	}
+
+	v.users[address] = publicKey
+
+	return nil
+}
+
+func (v *Verifier) PostVerify(ctx context.Context, sourceDir string, providerPriv *ecdsa.PrivateKey, user common.Address, deliverIndex uint64, taskFee string, nonce string) (*SettlementMetadata, error) {
 	plaintext, err := util.ZipAndGetContent(sourceDir)
 	if err != nil {
 		return nil, err
@@ -62,9 +137,9 @@ func (v *Verifier) Verify(ctx context.Context, sourceDir string, providerPriv *e
 		return nil, err
 	}
 
-	tagSig, err := crypto.Sign(tag[:], providerPriv)
+	tagSig, err := ethcrypto.Sign(tag[:], providerPriv)
 	if err != nil {
-		return nil, fmt.Errorf("failed to sign tag: %v", err)
+		return nil, errors.Wrap(err, "sign tag")
 	}
 
 	modelRootHash, err := util.UploadEncryptFile(sourceDir, ciphertext, tagSig)
@@ -77,9 +152,19 @@ func (v *Verifier) Verify(ctx context.Context, sourceDir string, providerPriv *e
 		return nil, err
 	}
 
-	encryptedSecret, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, key, nil)
+	return v.GenerateTeeSignature(user, key, modelRootHash, taskFee, nonce, providerPriv)
+}
+
+func (v *Verifier) GenerateTeeSignature(user common.Address, key []byte, modelRootHash []byte, taskFee string, nonce string, providerPriv *ecdsa.PrivateKey) (*SettlementMetadata, error) {
+	publicKey, ok := v.users[user]
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("public key for user %v not exist", user))
+	}
+
+	eciesPublicKey := ecies.ImportECDSAPublic(publicKey)
+	encryptedSecret, err := ecies.Encrypt(rand.Reader, eciesPublicKey, key, nil, nil)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "encrypting secret")
 	}
 
 	settlementHash, err := getSettlementMessageHash(modelRootHash, taskFee, nonce, user, encryptedSecret)
@@ -87,7 +172,7 @@ func (v *Verifier) Verify(ctx context.Context, sourceDir string, providerPriv *e
 		return nil, err
 	}
 
-	sig, err := crypto.Sign(accounts.TextHash(settlementHash[:]), providerPriv)
+	sig, err := ethcrypto.Sign(accounts.TextHash(settlementHash[:]), providerPriv)
 	if err != nil {
 		return nil, err
 	}
