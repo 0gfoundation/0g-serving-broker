f542775a995461c3499c80c279b3e4f88f6c3d41
Peter Zhang <peter@0g.ai>
2025-01-24 16:22:08 +0800

    add image script

---
+++ api/fine-tuning/internal/ctrl/constants.go
@@ -0,0 +1,7 @@
+package ctrl
+
+var ScriptMap = map[string]string{
+	"0x8645816c17a8a70ebf32bcc7e621c659e8d0150b1a6bfca27f48f83010c6d12e": "/app/finetune_img.py",
+	"0x7f2244b25cd2219dfd9d14c052982ecce409356e0f08e839b79796e270d110a7": "/app/finetune.py",
+	"0x2084fdd904c9a3317dde98147d4e7778a40e076b5b0eb469f7a8f27ae5b13e7f": "/app/finetune.py",
+}
