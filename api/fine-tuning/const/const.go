a02d2fde18d045393f044295b1363ead629e5cbd
Ravenyjh <jiahaoyan20172@gmail.com>
2025-03-03 11:31:33 +0800

    Refactor(fine-tuning): Enable image configuration support

--- api/fine-tuning/const/const.go
+++ api/fine-tuning/const/const.go
@@ -1,9 +1,6 @@
 package constant
 
 var (
-	EXECUTION_IMAGE_NAME      = "execution-test-pytorch"
-	EXECUTION_MOCK_IMAGE_NAME = "mock-fine-tuning"
-
 	MOCK_MODEL_ROOT_HASH = "0xcb42b5ca9e998c82dd239ef2d20d22a4ae16b3dc0ce0a855c93b52c7c2bab6dc"
 
 	// TODO: For MVP, this is hardcoded to true. In the future, this should can be configurable.
