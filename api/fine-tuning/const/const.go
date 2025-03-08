60273641941fcaf7009321fdb0c4537ad8e65dc9
Sidonie <sidonie@0g.ai>
2025-03-08 15:36:27 +0000

    Fix(ft): Update dockerfile and hash for cocktail

--- api/fine-tuning/const/const.go
+++ api/fine-tuning/const/const.go
@@ -11,11 +11,11 @@ var (
 		"0x7f2244b25cd2219dfd9d14c052982ecce409356e0f08e839b79796e270d110a7": "/app/finetune.py",
 		"0x2084fdd904c9a3317dde98147d4e7778a40e076b5b0eb469f7a8f27ae5b13e7f": "/app/finetune.py",
 		"0xcb42b5ca9e998c82dd239ef2d20d22a4ae16b3dc0ce0a855c93b52c7c2bab6dc": "/app/finetune.py",
-		"0xb8b7bacdf70588401b1315a607478b6c76f446f3683d6cc6f956909d64f35f8b": "/app/CocktailSGD/finetune-cocktail.py",
+		"0x02ed6d3889bebad9e2cd4008066478654c0886b12ad25ea7cf7d31df3441182e": "/app/CocktailSGD/finetune-cocktail.py",
 	}
 
 	ENV_MAP = map[string][]string{
-		"0xb8b7bacdf70588401b1315a607478b6c76f446f3683d6cc6f956909d64f35f8b": {
+		"0x02ed6d3889bebad9e2cd4008066478654c0886b12ad25ea7cf7d31df3441182e": {
 			"PYTHONPATH=/app/CocktailSGD", // Update to match Python version
 		}}
 )
