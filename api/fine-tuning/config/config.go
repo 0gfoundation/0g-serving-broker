132eb156f446392deff218b41119ba306e223d46
Ravenyjh <jiahaoyan20172@gmail.com>
2025-05-14 18:16:13 +0800

    feat: update Ethereum network configuration to use new RPC URL and chain ID

--- api/fine-tuning/config/config.go
+++ api/fine-tuning/config/config.go
@@ -170,7 +170,7 @@ func loadConfig(config *Config) error {
 func GetConfig() *Config {
 	once.Do(func() {
 		instance = &Config{
-			ContractAddress: "0xc0B71cA19184B678Fdd6eC729aa24f054E57c10c",
+			ContractAddress: "0x58a9842553Dada1BE9741ceB916eD5b7119d510e",
 			Database: struct {
 				FineTune string `yaml:"fineTune"`
 			}{
