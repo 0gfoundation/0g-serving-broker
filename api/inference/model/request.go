0e3bed367294cb416fd68fdabb5db716815674ab
Joel <liuyuanvic@gmail.com>
2025-06-11 09:05:38 +0000

    sign chat response locally

--- api/inference/model/request.go
+++ api/inference/model/request.go
@@ -12,6 +12,7 @@ type Request struct {
 	TeeSignature string `gorm:"type:varchar(255);not null" json:"teeSignature" binding:"required" immutable:"true"`
 	RequestHash  string `gorm:"type:varchar(255);not null;primaryKey" json:"requestHash" binding:"required" immutable:"true"`
 	Processed    bool   `gorm:"type:tinyint(1);not null;default:0;index:processed_userAddress_nonce" json:"processed"`
+	UseProxy     bool   `gorm:"type:tinyint(1);not null;default:0" json:"userProxy"`
 }
 
 type RequestList struct {
