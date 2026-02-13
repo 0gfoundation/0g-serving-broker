2ad73a5b33b34f7060b52e6088bc933ffec08e46
Ravenyjh <30866879+Ravenyjh@users.noreply.github.com>
2026-02-13 15:00:10 +0800

    chore: update config image sha256 to v0.4.0 (#363)

--- api/inference/integration/config/main.go
+++ api/inference/integration/config/main.go
@@ -287,7 +287,7 @@ const dockerComposeTemplate = `services:
 
   # Main broker service
   0g-serving-provider-broker:
-    image: ghcr.io/0gfoundation/0g-serving-broker@sha256:e4df3cdac0b01ce78327f862e13fdac69714dda11c784467bd72a9e4cabe86a5
+    image: ghcr.io/0gfoundation/0g-serving-broker@sha256:02f86cec7e827c16888e667fbcfa889aea7532a188df36ee06bd57375c9a89dd
 {{- if not .UseNginx}}
     ports:
       - "{{.Ports.Nginx80}}:3080"
@@ -361,7 +361,7 @@ const dockerComposeTemplate = `services:
 
   # Event service starts after broker is ready
   0g-serving-provider-event:
-    image: ghcr.io/0gfoundation/0g-serving-broker@sha256:e4df3cdac0b01ce78327f862e13fdac69714dda11c784467bd72a9e4cabe86a5
+    image: ghcr.io/0gfoundation/0g-serving-broker@sha256:02f86cec7e827c16888e667fbcfa889aea7532a188df36ee06bd57375c9a89dd
     environment:
       - CONFIG_FILE=/etc/config.yaml
 {{- if eq .TeeNode "hardhat"}}
@@ -411,7 +411,7 @@ const dockerComposeTemplate = `services:
 
 {{- if .UseController}}
   0g-controller:
-    image: ghcr.io/0gfoundation/0g-serving-broker@sha256:e4df3cdac0b01ce78327f862e13fdac69714dda11c784467bd72a9e4cabe86a5
+    image: ghcr.io/0gfoundation/0g-serving-broker@sha256:02f86cec7e827c16888e667fbcfa889aea7532a188df36ee06bd57375c9a89dd
 {{- if .ControllerExposePort}}
     ports:
       - "{{.ControllerPort}}:3090"
@@ -1092,7 +1092,7 @@ func generateYAMLConfig(originalDir string, deployLLM bool, targetTeeAddress str
 		config.Controller = ControllerConfig{
 			Enable:         true,
 			AdminAddresses: []string{controllerAdminAddress},
-			Image:          "ghcr.io/0gfoundation/0g-serving-broker@sha256:e4df3cdac0b01ce78327f862e13fdac69714dda11c784467bd72a9e4cabe86a5",
+			Image:          "ghcr.io/0gfoundation/0g-serving-broker@sha256:02f86cec7e827c16888e667fbcfa889aea7532a188df36ee06bd57375c9a89dd",
 		}
 	}
 
